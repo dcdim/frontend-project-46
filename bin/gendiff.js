0e443ffda474db6570e81c14ee59a0d480dd1d64
Дмитрий Каширин <dcdim@mail.ru>
2024-01-07 23:36:21 +0300

    Change bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ const program = new Command();
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filePath1> <filePath2>')
 
-  program.parse();
+program.parse();
